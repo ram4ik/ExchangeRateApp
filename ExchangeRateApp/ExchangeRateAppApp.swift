d7873e2d643996ce668c19fc69a2e805756f7267
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-09-13 18:28:42 +0300

    Initial Commit

---
+++ ExchangeRateApp/ExchangeRateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRateAppApp.swift
+//  ExchangeRateApp
+//
+//  Created by test on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
